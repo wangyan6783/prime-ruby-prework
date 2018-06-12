861d23e9c9581e2755d50b3df0ac795a1c7792be
Yan Wang <wangyan6783@gmail.com>
2018-06-12 15:09:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  end
+  i = 2
+  while i < num
+    if num % i == 0
+      return false
+    end
+    i += 1
+  end
+  true
+end
